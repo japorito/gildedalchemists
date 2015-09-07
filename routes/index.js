d952c52351d1bf30557ab044d960f51da4079f1e
Jacob Saporito <japorito@gmail.com>
2015-09-06 21:54:20 -0700

    Add pics of my character, and do some small work on the front page.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
+'use strict';
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Gilded Alchemists Guild' });
+  res.render('index', {
+    title: 'Gilded Alchemists Guild',
+    welcome_msg: 'Welcome to the Gilded Alchemists Guild website, where every rainbow is golden.'
+  });
 });
 
 module.exports = router;
